de66c017f52fcb2b0f9849fbf599d637761edce5
zaidraed <zaidraed@gmail.com>
2025-03-31 17:07:30 -0300

    deploy 13

--- server/src/orders/orders.module.ts
+++ server/src/orders/orders.module.ts
@@ -5,7 +5,7 @@ import { OrdersController } from './orders.controller';
 import { MoviesModule } from '../movies/movies.module';
 import { PrismaModule } from '../prisma/prisma.module';
 import { PaymentsModule } from '../payments/payments.module';
-import { ScreeningModule } from 'src/screening/screening.module';
+import { ScreeningModule } from '../screening/screening.module';
 
 @Module({
   controllers: [OrdersController],
