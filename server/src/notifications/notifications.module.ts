16fa979367c7ff84ea6aabd141bb44dcc1184f5e
zaidraed <zaidraed@gmail.com>
2025-03-31 16:55:05 -0300

    "deploy 11"

--- server/src/notifications/notifications.module.ts
+++ server/src/notifications/notifications.module.ts
@@ -1,14 +1,13 @@
-import { Module } from '@nestjs/common';
-import { NotificationsService } from './notifications.service';
-import { NotificationsController } from './notifications.controller';
+import { Module } from "@nestjs/common";
+import { NotificationsService } from "./notifications.service";
+import { NotificationsController } from "./notifications.controller";
 import { PrismaService } from "../prisma/prisma.service";
-import { PrismaModule } from 'src/prisma/prisma.module';
-
+import { PrismaModule } from "../prisma/prisma.module";
 
 @Module({
   imports: [PrismaModule],
   controllers: [NotificationsController],
   providers: [NotificationsService],
-  exports: [NotificationsService], 
+  exports: [NotificationsService],
 })
 export class NotificationsModule {}
