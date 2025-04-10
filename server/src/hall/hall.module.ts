4db1dca900790d51f7947082029db089ac880ea7
zaidraed <zaidraed@gmail.com>
2025-04-10 19:21:47 -0300

    fix hall module

--- server/src/hall/hall.module.ts
+++ server/src/hall/hall.module.ts
@@ -2,10 +2,11 @@ import { Module } from "@nestjs/common";
 import { HallService } from "./hall.service";
 import { HallController } from "./hall.controller";
 import { PrismaService } from "../prisma/prisma.service";
-
+import { SeatModule } from "../seat/seat.module";
 @Module({
+  imports: [SeatModule],
   controllers: [HallController],
   providers: [HallService, PrismaService],
-  exports: [HallService]
+  exports: [HallService],
 })
 export class HallModule {}
