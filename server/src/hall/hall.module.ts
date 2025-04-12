52665064ff0f4eca468c04eae39d093ed428a5b8
zaidraed <zaidraed@gmail.com>
2025-04-12 14:41:27 -0300

    fix stupid error from deep seek

--- server/src/hall/hall.module.ts
+++ server/src/hall/hall.module.ts
@@ -3,9 +3,8 @@ import { HallService } from "./hall.service";
 import { HallController } from "./hall.controller";
 import { PrismaService } from "../prisma/prisma.service";
 import { SeatModule } from "../seat/seat.module";
-import { PrismaModule } from "src/prisma/prisma.module";
 @Module({
-  imports: [PrismaModule, SeatModule],
+  imports: [SeatModule],
   controllers: [HallController],
   providers: [HallService, PrismaService],
   exports: [HallService],
--- server/src/seat/seat.module.ts
+++ server/src/seat/seat.module.ts
@@ -2,10 +2,8 @@ import { Module } from "@nestjs/common";
 import { SeatService } from "./seat.service";
 import { SeatController } from "./seat.controller";
 import { PrismaService } from "../prisma/prisma.service";
-import { PrismaModule } from "src/prisma/prisma.module";
 
 @Module({
-  imports: [PrismaModule],
   controllers: [SeatController],
   providers: [SeatService, PrismaService],
   exports: [SeatService],
